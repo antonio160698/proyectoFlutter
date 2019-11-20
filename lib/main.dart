84ef9038d86a293da1e4ae8803dab9c2f439b2f8
Antonio <antonio_1606_98@hotmail.com>
2019-11-19 20:02:58 -0600

    Se hizo lo de la camara con image_picker tanto se puede tomar una fgoto de la galeria y visualizarla como tomar una foto y visualizarla

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'Camara.dart';
+import 'Camara_1.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -91,6 +93,15 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            RaisedButton(
+              child:Text('Fist Route'),
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LandingScreen()),
+                );
+              },
+            ),
             Text(
               'You have pushed the button this many times:',
             ),
